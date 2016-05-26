a1a4499c7b17c4cba0f889b4ab4a2d2586d63ea5
Jesus Flores <joy_warmgun@hotmail.com>
2016-05-26 10:48:29 -0500

    jeje murio mi maquina, probando con schedule 	modified:   hilo.c

--- hilo.c
+++ hilo.c
@@ -1,4 +1,3 @@
-#include <unistd.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -12,7 +11,7 @@ int hilo_function(void *data)
 	while(1)
 	{
 		printk(KERN_INFO "Hilo:: Saludos desde funcion del hilo\n");
-		sleep(2);
+		schedule();
 	}
 	return 0;
 }
